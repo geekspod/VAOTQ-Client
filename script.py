42942e627e044502a0477d453c2319a0c70266bd
MZO9400 <hamza.hameed00@yahoo.com>
2022-05-31 22:23:51 +0500

    Cleanup main script

--- script.py
+++ script.py
@@ -1,23 +1,12 @@
 import os
-import pickle
-import socket
-import struct
 
-import cv2
-
-from video import Video
-from socket import Socket
+from modules import Client, Socket, Video
 
 video_stream = Video()
-video_stream.start()
 
-socket = Socket(os.environ['ip'], int(os.environ['port']))
+sock = Socket(os.environ['ip'], int(os.environ['port']))
 
-if socket.connect() == False:
-    raise Exception("Error occurred while connecting")
 
-while socket.connected:
-    frame = cv2.resize(video_stream.frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
-    data = pickle.dumps(frame)
-    message_size = struct.pack("L", len(data))
-    socket.send(message_size, data)
+client = Client(video_stream, sock)
+
+client.start()
