f47ea393bf24573995500955af840b1abff0628b
MZO9400 <hamza.hameed00@yahoo.com>
2022-05-31 21:28:47 +0500

    Implement socket class in main script

--- script.py
+++ script.py
@@ -6,24 +6,18 @@ import struct
 import cv2
 
 from video import Video
+from socket import Socket
 
 video_stream = Video()
 video_stream.start()
 
-connection = (os.environ['ip'], int(os.environ['port']))
+socket = Socket(os.environ['ip'], int(os.environ['port']))
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+if socket.connect() == False:
+    raise Exception("Error occurred while connecting")
 
-print("Connecting to {0}".format(connection))
-
-sock.connect(connection)
-
-while True:
-
+while socket.connected:
     frame = cv2.resize(video_stream.frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
-
     data = pickle.dumps(frame)
-
     message_size = struct.pack("L", len(data))
-
-    sock.sendall(message_size + data)
+    socket.send(message_size, data)
