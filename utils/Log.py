dd8e21c0bcc1e30b8029cb9b7b11373c54e49875
MZO9400 <hamza.hameed00@yahoo.com>
2022-05-31 23:53:29 +0500

    Log to files & stdout

--- utils/Log.py
+++ utils/Log.py
@@ -1,10 +1,31 @@
 import logging
+import os
+
+from utils.constants import LOGGER_FORMAT, LOGS_DIR
 
 
 class Log:
     def __init__(self, tag):
         self.tag = tag
-        logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+        logging.basicConfig(
+            level=logging.DEBUG,
+            format=LOGGER_FORMAT
+        )
+
+        self.create_logs_dir()
+        self.setup_logger()
+
+    @staticmethod
+    def create_logs_dir():
+        if not os.path.exists(LOGS_DIR):
+            os.makedirs(LOGS_DIR)
+
+    def setup_logger(self):
+        logger = logging.getLogger(self.tag)
+        file_handler = logging.FileHandler("{0}/{1}.log".format(LOGS_DIR, self.tag))
+        file_handler.setFormatter(logging.Formatter(LOGGER_FORMAT))
+        logger.addHandler(file_handler)
 
     def get_logger(self):
         return logging.getLogger(self.tag)
--- utils/constants.py
+++ utils/constants.py
@@ -1,1 +1,3 @@
 SOCKET_RETRY_DELAY = 3
+LOGGER_FORMAT = '%(asctime)s - %(threadName)-12.12s - %(name)s - %(levelname)s - %(message)s'
+LOGS_DIR = "logs"
